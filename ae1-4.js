1bd3b2b2be4019c4a1599c37c4f0abd19789b8b3
chema <jomoro01@floridauniversitaria.es>
2020-09-29 17:25:25 +0200

    completamos ejercicio 4

---
+++ ae1-4.js
@@ -0,0 +1,18 @@
+let numero1= 5;
+let numero2= 8;
+if (numero1 < numero2)
+{
+    console.log('El número1 no es mayor que número2')
+}
+if (numero2 >= 0)
+{
+    console.log('El número2 es positivo')
+}
+if (numero1 < 0||numero1 != 0)
+{
+    console.log('El número1 es negativo o distinto de 0')
+}
+if (++numero1 <= numero2)
+{
+    console.log('incrementar en una unidad el valor de número1 no lo hace  mayor  o igual que número2')
+}
