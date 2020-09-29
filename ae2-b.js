ac4e27f73d2a785a4a4c69816a7130115a44e964
chema <jomoro01@floridauniversitaria.es>
2020-09-29 16:30:09 +0200

    completamos el apartado b

---
+++ ae2-b.js
@@ -0,0 +1,5 @@
+let MyArray =["enero","febrero",'marzo','abril','mayo','junio',
+'julio','agosto','septiembre','octubre','noviembre','diciembre'];
+for (let values of MyArray){
+    console.log(values);
+}
